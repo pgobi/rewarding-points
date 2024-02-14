60b7070316c737a1ca206358438d90aa5d3f0fc7
pgobi <160076473+pgobi@users.noreply.github.com>
2024-02-14 23:33:43 +0100

    init project

---
+++ src/main/java/com/pgobi/rewardingpoints/RewardingPointsApplication.java
@@ -0,0 +1,13 @@
+package com.pgobi.rewardingpoints;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RewardingPointsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RewardingPointsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pgobi/rewardingpoints/RewardingPointsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pgobi.rewardingpoints;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RewardingPointsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
